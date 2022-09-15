07be4dd788a637b3ab9a3873f6965d50cb50e656
WickTheThird <96390895+WickTheThird@users.noreply.github.com>
2022-09-15 20:58:38 +0100

    whatever this is its done

---
+++ test.r
@@ -0,0 +1,4 @@
+downtime <- c(0, 1, 2, 12, 12, 14, 18, 21, 21, 23, 24, 25, 28, 29, 30, 30,
+              33, 36, 44, 45, 47, 51)
+
+results <- read.table('~/Documents/GitHub/CA266/results.txt', header = T)
