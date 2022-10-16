a793797931cf3bf02f2ec2ba78c45450d34f26b4
WickTheThird <filip.bumbu2@mail.dcu.ie>
2022-10-16 16:16:42 +0100

    I have tested some things in R Studio

--- test.r
+++ test.r
@@ -1,4 +1,40 @@
-downtime <- c(0, 1, 2, 12, 12, 14, 18, 21, 21, 23, 24, 25, 28, 29, 30, 30,
-              33, 36, 44, 45, 47, 51)
+# Introduction to R
 
-results <- read.table('~/Documents/GitHub/CA266/results.txt', header = T)
+# Assigning variables
+
+x <- 7 # or 7 -> x, but we prefer the first way
+print(7)
+
+# We can assign a range of variables
+
+x <- 1:4 # We can multiply this range by a number
+print(x)
+b <- x*10
+print(b)
+
+# We can get fast help in R by typing
+# either help.start() (this opening the entire docs)
+# or ?(name_of_function) or in case you vaguely know
+# the name of the function you can write apropos("vague_name")
+# then a list of possible names will apear and if you forgot what it
+# does then just somply use "?"
+
+apropos("att")
+
+?attach()
+
+# You can also assign data 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
